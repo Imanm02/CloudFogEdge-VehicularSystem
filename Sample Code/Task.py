b16180b868d6bfbeafe9582cc5b722e0214a16dc
Negar <babashahNegar@gmail.com>
2024-06-28 18:04:17 +0330

    Fix bug in getting tasks

--- Sample Code/Task.py
+++ Sample Code/Task.py
@@ -8,14 +8,14 @@ def get_needed_freq(cpu_cycles, deadline):
 
 
 class Task:
-    def __init__(self, cpu_cycles, name, size, deadline, owner_node, creation_time):
+    def __init__(self, cpu_cycles, name, size, deadline, creator, creation_time):
         self.cpu_cycles = cpu_cycles
         self.name = name
         self.deadline = deadline
         self.size = size
         self.creation_time = creation_time
         self.needed_freq = get_needed_freq(cpu_cycles, deadline)
-        self.owner_node = owner_node
+        self.creator = creator
         self.assigned_node = None
 
     def set_assignee(self, node):
